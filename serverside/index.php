de6d44eb1377316fe23111345df787306e90fb75
Trex-Jet-Technology <143239104+Trex-Jet-Technology@users.noreply.github.com>
2023-08-27 20:48:56 +0100

    Add files via upload

---
+++ serverside/index.php
@@ -0,0 +1,8 @@
+<?php
+//function
+var(
+    <index class="html"></index>
+    <import_request_variables {contact.html_entity_decode}
+    [file_exists(feature.htmlentities)]
+msg_stat_queue(file_get_contents{index.html})
+)
